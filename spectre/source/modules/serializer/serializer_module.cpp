eb2e5348c0e01940b2b32d6cdf628735c1658395
jehud <jehudmetellus@outlook.com>
2026-07-09 10:24:17 -0400

    Implement missing serializer_module logic

--- spectre/source/modules/serializer/serializer_module.cpp
+++ spectre/source/modules/serializer/serializer_module.cpp
@@ -1,4 +1,7 @@
 #include "serializer_module.h"
+
+#include <format>
+
 #include "spectre/services/serializer_service.h"
 #include <iostream>
 
@@ -17,27 +20,41 @@ namespace spectre::modules {
     })
 
     serializer_module::serializer_module(flecs::world& world) : m_world(world) {
-        std::cout << "[SPECTRE] Initializing " << "serializer" << " module" << std::endl;
+        m_serializer = m_world.entity("::serializer");
     }
     
     serializer_module::~serializer_module() = default;
 
     void serializer_module::register_serializer(std::string_view type, const serializer_t& serializer) {
+        auto  entity = m_world.entity(type.data()).set<serializer_t>(serializer);
+
+        m_serializer.child(entity);
+
     }
 
     bool serializer_module::has_serializer(std::string_view type) const {
-        return false;
+        return find_serializer(type).is_valid();
     }
 
     flecs::entity serializer_module::find_serializer(std::string_view type) const {
-        return flecs::entity::null();
+        return m_serializer.lookup(type.data());
     }
 
     sandbox::properties serializer_module::serialize_entity(flecs::entity serializer, flecs::entity entity) {
+        if (!serializer.is_valid() || !entity.is_valid()) return sandbox::properties{};
+        const serializer_t* s = &serializer.get<serializer_t>();
+        if (s && s->serialize) {
+            return sandbox::properties{s->serialize(m_world.c_ptr(), entity.id())};
+        }
         return sandbox::properties{};
     }
 
     flecs::entity serializer_module::deserialize_entity(flecs::entity serializer, const sandbox::properties& props) {
+        if (!serializer.is_valid()) return flecs::entity::null();
+        const serializer_t* s = &serializer.get<serializer_t>();
+        if (s && s->deserialize) {
+            return flecs::entity(m_world, s->deserialize(m_world.c_ptr(), props.get_raw()));
+        }
         return flecs::entity::null();
     }
 
